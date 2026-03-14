ae9bf676177c914df05bad0f6852e943a687d797
Ozancan Direk <ozan_direk@hotmail.com>
2026-03-14 18:19:50 +0300

    signalR kutuphanesi dahil edildi

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.Builder;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.BusinessLayer.Concrete;
 using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.EntityFramework;
+using SignalRApi.Hubs;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+               .AllowAnyMethod()
+               .SetIsOriginAllowed(host => true)
+               .AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
 
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
@@ -51,6 +65,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.UseHttpsRedirection();
 
