70d61eaa49d60fa6376813cedb044c012e9a82b7
Ozancan Direk <ozan_direk@hotmail.com>
2026-03-21 20:56:43 +0300

    ana sayfa tasarimi yapildi apiler consume edildi

---
+++ SignalR.BusinessLayer/Concrete/SliderManager.cs
@@ -0,0 +1,41 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntitytLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class SliderManager : ISliderService
+    {
+        private readonly ISliderDal _sliderDal;
+
+        public SliderManager(ISliderDal sliderDal)
+        {
+            _sliderDal = sliderDal;
+        }
+
+        public void TAdd(Slider entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Slider entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Slider TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Slider> TGetListAll()
+        {
+            return _sliderDal.GetListAll();
+        }
+
+        public void TUpdate(Slider entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SignalR.DataAccessLayer/EntityFramework/EfSliderDal.cs
@@ -0,0 +1,14 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntitytLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfSliderDal : GenericRepository<Slider>, ISliderDal
+    {
+        public EfSliderDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
