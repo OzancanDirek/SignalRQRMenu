96c0cc365de7c871b1b516009e76c8dca904826d
Ozancan Direk <ozan_direk@hotmail.com>
2026-03-27 21:23:18 +0300

    signalR ile admin paneli baglandi rezervasyonlar dinamik sekilde goruntuleniyor

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -10,14 +10,16 @@ namespace SignalRApi.Hubs
         private readonly IOrderService _orderService;
         private readonly IMoneyCaseService _moneyCaseService;
         private readonly IMenuTableService _menuTableService;
+        private readonly IBookingService _bookingService;
 
-        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService)
         {
             _categoryService = categoryService;
             _productService = productService;
             _orderService = orderService;
             _moneyCaseService = moneyCaseService;
             _menuTableService = menuTableService;
+            _bookingService = bookingService;
         }
 
 
@@ -70,9 +72,6 @@ namespace SignalRApi.Hubs
 
             var value16 = _menuTableService.TTableCount();
             await Clients.All.SendAsync("ReceiveTableCount", value16);
-
-
-
         }
 
         public async Task SendProgress()
@@ -85,6 +84,13 @@ namespace SignalRApi.Hubs
 
             var value3 = _menuTableService.TTableCount();
             await Clients.All.SendAsync("ReceiveTableCount", value3);
+
+        }
+
+        public async Task GetBookingList()
+        {
+            var values = _bookingService.TGetListAll();
+            await Clients.All.SendAsync("ReceiveBookingList", values);
 
         }
     }
