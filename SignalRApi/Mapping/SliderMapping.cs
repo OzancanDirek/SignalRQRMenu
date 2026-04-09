fa2c015ee75220101cb14e55090ed11dd09a4046
Ozancan Direk <ozan_direk@hotmail.com>
2026-04-09 22:48:03 +0300

    UI tarafindaki revizeler yapildi bug fixler gerceklestirildi

---
+++ SignalRApi/Mapping/SliderMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.SliderDto;
+using SignalR.EntitytLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class SliderMapping : Profile
+    {
+        public SliderMapping()
+        {
+            CreateMap<Slider,ResultSliderDto>().ReverseMap();
+        }
+    }
+}
