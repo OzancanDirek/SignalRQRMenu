4ceed92af58b590e4a5955fd3fc10f9bb4917bc1
Ozancan Direk <ozan_direk@hotmail.com>
2026-04-20 17:24:47 +0300

    yemek tarifleri videolari ve fluentvalidation projeye dahil edildi

---
+++ SignalRWebUI/Dtos/ValidationRulesDtos/CreateBookingValidation.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using SignalRWebUI.Dtos.BookingDtos;
+
+namespace SignalRWebUI.ValidationRules
+{
+    public class CreateBookingValidation : AbstractValidator<CreateBookingDto>
+    {
+        public CreateBookingValidation()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("İsim alanı boş bırakılamaz")
+                .MinimumLength(5).WithMessage("En az 5 karakter giriniz")
+                .MaximumLength(50).WithMessage("En fazla 50 karakter giriniz");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty().WithMessage("Telefon alanı boş bırakılamaz");
+
+            RuleFor(x => x.Mail)
+                .NotEmpty().WithMessage("Mail alanı boş bırakılamaz")
+                .EmailAddress().WithMessage("Geçerli mail giriniz");
+
+            RuleFor(x => x.PersonCount)
+                .NotEmpty().WithMessage("Kişi sayısı boş bırakılamaz");
+
+            RuleFor(x => x.Date)
+                .NotEmpty().WithMessage("Tarih boş bırakılamaz");
+        }
+    }
+}
